b209300320e02608a2d6e9c70188d6c38b11b8ca
hs@DEVELOPER02 <h.schindler@qs-schindler.de>
2020-06-18 14:36:37 +0200

    - changed deprecated requireCodovaModule to require

--- scripts/update_build_gradle.js
+++ scripts/update_build_gradle.js
@@ -3,8 +3,10 @@ module.exports = function (context) {
         return;
     }
     console.log("Starting gradle modifications");
-    const path = context.requireCordovaModule('path');
-    const fs = context.requireCordovaModule('fs');
+    // const path = context.requireCordovaModule('path');
+    // const fs = context.requireCordovaModule('fs');
+    const path = require('path');
+    const fs = require('fs');
     const gradlePath = path.join(context.opts.projectRoot, 'platforms/android/app/build-extras.gradle');
     const gradleExtraPath = path.join(context.opts.projectRoot, 'plugins/c4-gun-api-cordova-plugin/build-extras.gradle');
     return new Promise(function (resolve, reject) {
